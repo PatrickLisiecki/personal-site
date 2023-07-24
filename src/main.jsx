d9b1623952017cbad3baed7787fcb21285bbc834
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-23 23:19:39 -0400

    Removed routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ErrorPage from "./components/ErrorPage";
-import About from "./components/About";
-import Home from "./components/Home";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -14,24 +10,6 @@ const router = createBrowserRouter([
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/projects",
-                element: <Projects />,
-            },
-            {
-                path: "/contact",
-                element: <Contact />,
-            },
-        ],
     },
 ]);
 

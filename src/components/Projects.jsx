d275bb8d3cc08996c4732b9273958884bc5a757a
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-25 02:04:41 -0400

    Updated projects section and linked projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,71 @@
+/* eslint-disable react/jsx-key */
+
 // Icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Projects data
 const projectsData = [
     {
-        genre: "Frontend",
         title: "Book Review Forum",
-        link: "https://github.com/PatrickLisiecki/portfolio",
-        image: "src/assets/project.png",
+        description:
+            "A functional dynamic forum that gives users the option to create, edit, and delete forums, as well as a thread creation feature.",
+        link: "https://github.com/PatrickLisiecki/book-review-forum",
+        image: "src/assets/project-1.png",
+        demo: "#",
+        tech: [
+            <i className="devicon-html5-plain colored"></i>,
+            <i className="devicon-react-original colored"></i>,
+            <i className="devicon-tailwindcss-plain colored"></i>,
+        ],
     },
     {
-        genre: "Frontend",
-        title: "Book Review Forum",
-        link: "https://github.com/PatrickLisiecki/portfolio",
-        image: "src/assets/project.png",
+        title: "Flashcard Creator",
+        description:
+            "A user-friendly and interactive application for effective learning with digital flashcards.",
+        link: "https://github.com/PatrickLisiecki/flashcard-creator",
+        demo: "#",
+        image: "src/assets/project-2.png",
+        tech: [
+            <i className="devicon-mongodb-plain colored"></i>,
+            <i className="devicon-express-original colored"></i>,
+            <i className="devicon-react-original colored"></i>,
+            <i className="devicon-nodejs-plain colored"></i>,
+        ],
     },
     {
-        genre: "Frontend",
-        title: "Book Review Forum",
+        title: "Portfolio Website",
+        description: "An interactive portfolio website for a developer.",
         link: "https://github.com/PatrickLisiecki/portfolio",
-        image: "src/assets/project.png",
-    },
-    {
-        genre: "Frontend",
-        title: "Book Review Forum",
-        link: "https://github.com/PatrickLisiecki/portfolio",
-        image: "src/assets/project.png",
+        demo: "https://portfolio-patricklisiecki.vercel.app/",
+        image: "src/assets/project-3.png",
+        tech: [
+            <i className="devicon-nextjs-original colored"></i>,
+            <i className="devicon-react-original colored"></i>,
+            <i className="devicon-tailwindcss-plain colored"></i>,
+        ],
     },
 ];
 
 export default function Projects() {
     return (
-        <div className="section" id="projects">
+        <div className="py-28 lg:min-h-screen flex items-center" id="projects">
             <div className="container mx-auto">
                 <div className="flex flex-col justify-center items-center justify-center">
                     <div className="flex flex-col justify-center items-center text-center mb-4 xl:w-[35vw]">
+                        <div className="w-full flex justify-center items-center">
+                            <img
+                                src="src/assets/computer.png"
+                                alt="Case"
+                                className="max-w-[75px] max-h-[75px] md:max-w-[100px] md:max-h-[100px]"
+                            />
+                        </div>
                         <span className="h2 capitalize mb-4">My Latest Work</span>
-                        <p className="max-w-[400px] mx-auto mb-4 text-center text-secondary">
+                        {/* <p className="max-w-[400px] mx-auto mb-4 text-center text-secondary">
                             A collection of recent projects I have worked on. Click on a project to
                             view more details.
-                        </p>
+                        </p> */}
                     </div>
-                    <div className="w-[75%]">
+                    {/* <div className="w-[75%]">
                         <div className="grid grid-cols-1 grid-rows-4 gap-4 cursor-pointer lg:grid-cols-2 lg:grid-rows-2">
                             {projectsData.map((item, index) => {
                                 return (
@@ -52,12 +76,12 @@ export default function Projects() {
                                         className="block"
                                         key={index}
                                     >
-                                        <div className="bg-gray-200 p-4 flex items-center justify-center">
-                                            <div className="flex items-center justify-center relative overflow-hidden group">
+                                        <div className="flex items-center justify-center">
+                                            <div className="rounded-lg flex items-center justify-center relative overflow-hidden group">
                                                 <img
                                                     src={item.image}
                                                     alt="Project Image"
-                                                    className="group-hover:scale-125 transition-all duration-500"
+                                                    className="rounded-lg group-hover:scale-125 transition-all duration-500"
                                                 />
                                                 <div className="absolute inset-0 bg-gradient-to-l from-transparent via-gray-400 to-gray-500 opacity-0 group-hover:opacity-80 transition-all duration-300"></div>
                                                 <div className="absolute bottom-0 translate-y-full text-[13px] tracking-[0.2em] group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
@@ -79,6 +103,77 @@ export default function Projects() {
                                 );
                             })}
                         </div>
+                    </div> */}
+
+                    <div className="w-full h-auto grid grid-cols-1 gap-10">
+                        {projectsData.map((item, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="w-full min-h-[300px] md:min-h-[350px] p-5 flex flex-col lg:flex-row lg:justify-center lg:items-center gap-x-8 shadow-bs rounded-[2rem]"
+                                >
+                                    {/* Project Image */}
+                                    <div className="min-w-[100%] h-full flex justify-center items-center rounded-lg overflow-hidden shadow-bs mb-2 xl:mb-0 lg:min-w-[60%]">
+                                        <img
+                                            src={item.image}
+                                            alt="Project Image"
+                                            className="h-full max-w-full object-fit rounded-lg hover:scale-125 transition-all duration-500"
+                                        />
+                                    </div>
+
+                                    {/* Project Info */}
+                                    <div className="w-full h-full mx-auto flex flex-col justify-center items-center">
+                                        {/* Project Title */}
+                                        <span className="h3 mb-2">{item.title}</span>
+
+                                        {/* Project Description */}
+                                        <p className="text-secondary text-center max-w-[400px] leading-tight mb-4">
+                                            {item.description}
+                                        </p>
+
+                                        {/* Project Tech */}
+                                        <div className="w-[50%] flex justify-evenly items-center gap-x-4 mb-8">
+                                            {item.tech.map((item, index) => {
+                                                return (
+                                                    <div
+                                                        key={index}
+                                                        className="w-[50px] h-[50px] p-4 flex justify-center items-center rounded-full shadow-bs text-3xl"
+                                                    >
+                                                        {item}
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+
+                                        {/* Project Links */}
+                                        <div className="w-full flex justify-center items-center gap-x-8">
+                                            <a href={item.link} target="_blank" rel="noreferrer">
+                                                <button className="bg-accent border border-accent min-w-[120px] max-w-[140px] md:max-w-[160px] px-4 py-3 transition-all duration-300 flex items-center justify-center overflow-hidden group">
+                                                    <span className="text-white group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                                                        Code
+                                                    </span>
+                                                    <FontAwesomeIcon
+                                                        icon="fa-solid fa-right-long"
+                                                        className="text-white -translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
+                                                    />
+                                                </button>
+                                            </a>
+                                            <a href={item.demo} target="_blank" rel="noreferrer">
+                                                <button className="bg-none border border-primary min-w-[120px] max-w-[140px] md:max-w-[160px] px-4 py-3 transition-all duration-300 flex items-center justify-center overflow-hidden group">
+                                                    <span className="text-primary group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                                                        Demo
+                                                    </span>
+                                                    <FontAwesomeIcon
+                                                        icon="fa-solid fa-right-long"
+                                                        className="text-primary -translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
+                                                    />
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>

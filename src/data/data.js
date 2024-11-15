56ce977423d0c0c0ecb12c3af1ebe01d4ca15414
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-15 18:28:45 -0500

    chore: export data to a separate file

---
+++ src/data/data.js
@@ -0,0 +1,145 @@
+export const navData = [
+    {
+        title: "Home",
+        link: "#home",
+    },
+    {
+        title: "About",
+        link: "#about",
+    },
+    {
+        title: "Skills",
+        link: "#skills",
+    },
+    {
+        title: "Projects",
+        link: "#projects",
+    },
+    {
+        title: "Contact",
+        link: "#contact",
+    },
+];
+
+export const projectsData = [
+    {
+        title: "Spelling Bee App",
+        description: "A multilingual study assistant for expanding vocabulary with randomly generated games.",
+        link: "https://github.com/PatrickLisiecki/spelling-bee-app",
+        demo: "https://github.com/PatrickLisiecki/spelling-bee-app",
+        img: "/word-lab.png",
+        tech: ["/react.svg", "/typescript.svg", "/node.svg", "sqlite.svg"],
+        type: "web",
+    },
+    {
+        title: "Flex Fusion",
+        description: "A fitness application designed to provide users with a personalized fitness experience.",
+        link: "https://github.com/PatrickLisiecki/flex-fusion",
+        demo: "https://flexfusion.xyz/",
+        img: "/flex.png",
+        tech: ["/react.svg", "/tailwind.svg", "/node.svg", "postgresql.svg", "aws.svg"],
+        type: "web",
+    },
+    {
+        title: "Flashcard Creator",
+        description: "A user-friendly and interactive application for effective learning with digital flashcards.",
+        link: "https://github.com/PatrickLisiecki/flashcard-creator",
+        demo: "",
+        img: "/flashcard.png",
+        tech: ["/typescript.svg", "/react.svg", "node.svg", "mongodb.svg"],
+        type: "web",
+    },
+    {
+        title: "Portfolio Website",
+        description: "An interactive portfolio website for a developer built with Next.js and Framer Motion.",
+        link: "https://github.com/PatrickLisiecki/portfolio",
+        demo: "https://portfolio-patricklisiecki.vercel.app/",
+        img: "/dev.png",
+        tech: ["next.svg", "/typescript.svg", "react.svg"],
+        type: "web",
+    },
+    {
+        title: "Novel Opinions",
+        description: "A functional dynamic book forum.",
+        link: "https://github.com/PatrickLisiecki/book-review-forum",
+        demo: "",
+        img: "/novel.png",
+        tech: ["/react.svg", "/tailwind.svg"],
+        type: "web",
+    },
+];
+
+export const skillsData = [
+    {
+        title: "TypeScript",
+        type: "Programming Language",
+        img: "/typescript.svg",
+        link: "https://www.typescriptlang.org/",
+    },
+    {
+        title: "React",
+        type: "JavaScript Library",
+        img: "/react.svg",
+        link: "https://react.dev/",
+    },
+    {
+        title: "Amazon Web Services",
+        type: "Cloud Computing",
+        img: "/aws.svg",
+        link: "https://aws.amazon.com/",
+    },
+    {
+        title: "Python",
+        type: "Programming Language",
+        img: "/python.svg",
+        link: "https://www.python.org/",
+    },
+    {
+        title: "Node.js",
+        type: "JavaScript Runtime",
+        img: "/node.svg",
+        link: "https://nodejs.org/",
+    },
+    {
+        title: "Express.js",
+        type: "Web Framework",
+        img: "/express.svg",
+        link: "https://expressjs.com/",
+    },
+    {
+        title: "PostgreSQL",
+        type: "Database",
+        img: "/postgresql.svg",
+        link: "https://www.postgresql.org/",
+    },
+    {
+        title: "SQLite",
+        type: "Database",
+        img: "sqlite.svg",
+        link: "https://www.sqlite.org/",
+    },
+    {
+        title: "MongoDB",
+        type: "NoSQL Database",
+        img: "/mongodb.svg",
+        link: "https://www.mongodb.com/",
+    },
+    {
+        title: "Docker",
+        type: "Container Development",
+        img: "/docker.svg",
+        link: "https://www.docker.com/",
+    },
+    {
+        title: "Tailwind",
+        type: "CSS Framework",
+        img: "/tailwind.svg",
+        link: "https://tailwindcss.com/",
+    },
+    {
+        title: "Git",
+        type: "Version Control",
+        img: "/git.svg",
+        link: "https://git-scm.com/",
+    },
+];

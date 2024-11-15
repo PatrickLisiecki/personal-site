bd11477782ca97bee156eabf62efff7ee5b477fc
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-15 18:32:09 -0500

    refactor: refactor App to pass data as props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 // Components
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import LeftAside from "@/components/LeftAside";
+import RightAside from "@/components/RightAside";
 
 // Sections
 import Home from "@/components/sections/Home";
@@ -11,6 +13,11 @@ import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
 import Skills from "@/components/sections/Skills";
 
+// Data
+import { navData } from "@/data/data";
+import { skillsData } from "@/data/data";
+import { projectsData } from "@/data/data";
+
 function App() {
     const [darkMode, setDarkMode] = useState(true);
 
@@ -18,39 +25,23 @@ function App() {
         setDarkMode(!darkMode);
     };
 
-    // Toggle dark mode across the document
     useEffect(() => {
-        // Apply the selected mode's class to the root HTML element
         document.documentElement.classList.toggle("dark", darkMode);
     }, [darkMode]);
 
     return (
         <main className="page relative">
-            <Header />
+            <Header navData={navData} />
             <div className="flex flex-row">
-                <aside className="dark:border-divider sticky top-0 hidden h-screen w-20 flex-col items-center justify-center gap-6 border-r-2 border-secondary backdrop-blur-lg dark:bg-dark md:flex"></aside>
+                <LeftAside />
                 <div className="flex w-full flex-col">
                     <Home />
                     <About />
-                    <Skills />
-                    <Projects />
+                    <Skills skillsData={skillsData} />
+                    <Projects projectsData={projectsData} />
                     <Contact />
                 </div>
-                <aside className="dark:border-divider sticky top-0 hidden h-screen w-20 flex-col items-center justify-center gap-6 border-l-2 border-secondary backdrop-blur-lg dark:bg-dark md:flex">
-                    <button
-                        onClick={toggleDarkMode}
-                        className={`${
-                            darkMode
-                                ? "bg-white text-black hover:text-blue-500"
-                                : "bg-black text-white hover:text-blue-500"
-                        } grid h-[50px] w-[50px] place-items-center rounded-xl`}
-                    >
-                        <span>X</span>
-                    </button>
-                    <span className="absolute right-[50px] top-1/2 mr-[5px] min-w-[150px] -translate-y-1/2 rounded bg-black p-2 text-center text-sm text-white opacity-0 shadow transition-opacity group-hover:opacity-100">
-                        Toggle Dark Mode
-                    </span>
-                </aside>
+                <RightAside toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
             </div>
             <Footer />
 

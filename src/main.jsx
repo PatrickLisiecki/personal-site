38e8bd2cbd12a57dc8b0f77e049fe9e020cacae8
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-15 14:20:42 -0500

    refactor: update imports to align with new component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import ErrorPage from "./components/ErrorPage";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from "@/components/sections/ErrorPage";
 
 const router = createBrowserRouter([
     {

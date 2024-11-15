ef9fd0099218bd5b8009f9bce2ab160d7b1964b3
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-15 18:31:08 -0500

    feat: add left aside component (no functionality yet)

---
+++ src/components/LeftAside.jsx
@@ -0,0 +1,5 @@
+export default function LeftAside() {
+    return (
+        <aside className="dark:bg-background border-divider-secondary dark:text-neutral-dark-primary dark:border-divider-primary text-neutral-light-primary sticky top-0 hidden h-screen w-20 flex-col items-center justify-center gap-6 border-r-2 backdrop-blur-lg md:flex"></aside>
+    );
+}

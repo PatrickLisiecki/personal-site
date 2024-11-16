edb71e17a988c04254661d0dd270f3595be205d8
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-15 20:59:46 -0500

    add experience section and mobile dark mode control

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ import RightAside from "@/components/RightAside";
 
 import Home from "@/components/sections/Home";
 import About from "@/components/sections/About";
+import Experience from "@/components/sections/Experience";
+import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
-import Skills from "@/components/sections/Skills";
 
 import { navData } from "@/data/data";
+import { experienceData } from "@/data/data";
 import { skillsData } from "@/data/data";
 import { projectsData } from "@/data/data";
+
+import { Sun, Moon } from "lucide-react";
 
 function App() {
     const [darkMode, setDarkMode] = useState(true);
@@ -34,6 +38,7 @@ function App() {
                 <div className="flex w-full flex-col">
                     <Home />
                     <About />
+                    <Experience experienceData={experienceData} />
                     <Skills skillsData={skillsData} />
                     <Projects projectsData={projectsData} />
                     <Contact />
@@ -42,20 +47,18 @@ function App() {
                 <RightAside toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
             </div>
 
-            {/* Dark mode toggle */}
-            {/* <div className="group fixed bottom-[15px] right-[15px] rounded-full bg-none shadow-bs">
+            <div className="fixed bottom-[20px] right-[20px] z-50 sm:hidden">
                 <button
                     onClick={toggleDarkMode}
                     className={`${
-                        darkMode ? "bg-white text-black hover:text-blue-500" : "bg-black text-white hover:text-blue-500"
-                    } grid h-[50px] w-[50px] place-items-center rounded-full`}
+                        darkMode
+                            ? "bg-white text-black hover:text-yellow-500"
+                            : "bg-black text-white hover:text-blue-500"
+                    } grid h-[50px] w-[50px] place-items-center rounded-full shadow-lg transition-all duration-300`}
                 >
-                    <FontAwesomeIcon icon={faMoon} />
+                    {darkMode ? <Sun /> : <Moon />}
                 </button>
-                <span className="absolute right-[50px] top-1/2 mr-[5px] min-w-[150px] -translate-y-1/2 rounded bg-black p-2 text-center text-sm text-white opacity-0 shadow transition-opacity group-hover:opacity-100">
-                    Toggle Dark Mode
-                </span>
-            </div> */}
+            </div>
         </main>
     );
 }

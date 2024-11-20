2f157fc655cbea1421abb2050fd66e89cf8cd845
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-11-20 17:49:41 -0500

    format with Prettier

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -4,38 +4,37 @@ import { Users, Mail, Map, MessageSquare } from "lucide-react";
 export default function Contact() {
     return (
         <section
-            className="section dark:bg-background dark:text-neutral-dark-primary text-neutral-light-primary"
+            className="section text-neutral-light-primary dark:bg-background dark:text-neutral-dark-primary"
             id="contact"
         >
             <div className="container mx-auto">
                 <div className="mb-2 flex items-center gap-x-4">
                     <MessageSquare strokeWidth={3} size={26} />
-
                     <h3 className="text-[26px] font-semibold uppercase leading-[46px]">Let's Connect</h3>
                 </div>
 
-                <p className="dark:text-neutral-dark-secondary text-neutral-light-secondary mb-8">
+                <p className="mb-8 text-neutral-light-secondary dark:text-neutral-dark-secondary">
                     I'm always interested in working with others.
                 </p>
 
                 <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
-                    <div className="dark:hover:bg-muted flex items-center justify-between rounded-xl border p-4 hover:bg-gray-100">
+                    <div className="flex items-center justify-between rounded-xl border p-4 hover:bg-gray-100 dark:hover:bg-muted">
                         <div className="flex items-center gap-4">
                             <Map size={40} />
                             <div>
                                 <span className="text-[16px]">Location</span>
-                                <p className="dark:text-neutral-dark-secondary text-neutral-light-secondary text-[14px]">
+                                <p className="text-[14px] text-neutral-light-secondary dark:text-neutral-dark-secondary">
                                     New York, NY
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="dark:hover:bg-muted flex items-center justify-between rounded-xl border p-4 hover:bg-gray-100">
+                    <div className="flex items-center justify-between rounded-xl border p-4 hover:bg-gray-100 dark:hover:bg-muted">
                         <div className="flex items-center gap-4">
                             <Mail size={40} />
                             <div>
                                 <span className="text-[16px]">Email</span>
-                                <p className="dark:text-neutral-dark-secondary text-neutral-light-secondary text-[14px]">
+                                <p className="text-[14px] text-neutral-light-secondary dark:text-neutral-dark-secondary">
                                     impatricklisiecki@gmail.com
                                 </p>
                             </div>
@@ -45,13 +44,13 @@ export default function Contact() {
                         href="https://www.linkedin.com/in/patricklisiecki/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="dark:hover:bg-muted group flex items-center justify-between rounded-xl border p-4 transition-colors duration-200 hover:bg-gray-100"
+                        className="group flex items-center justify-between rounded-xl border p-4 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-muted"
                     >
                         <div className="flex items-center gap-4">
                             <Users size={40} />
                             <div>
                                 <span className="text-[16px]">LinkedIn</span>
-                                <p className="dark:text-neutral-dark-secondary text-neutral-light-secondary text-[14px]">
+                                <p className="text-[14px] text-neutral-light-secondary dark:text-neutral-dark-secondary">
                                     linkedin.com/in/patricklisiecki
                                 </p>
                             </div>
